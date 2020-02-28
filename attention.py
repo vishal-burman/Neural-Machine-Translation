b61d12e27857075113732c30860e44fadb2f1b43
Vishal Burman <vishal.a.burman23@gmail.com>
2020-02-28 15:57:49 +0530

    Modified attention

--- attention.py
+++ attention.py
@@ -4,3 +4,14 @@ class Attention:
 
         self.attn=nn.Linear((enc_hid_dim*2)+dec_hid_dim, dec_hid_dim)
         self.v=nn.Linear(dec_hid_dim, 1, bias=False)
+
+        def forward(self, hidden, encoder_outputs):
+
+            # hidden=[batch_size, dec_hid_dim]
+            # encoder_outputs=[src_len, batch_size, enc_hid_dim*2]
+
+            batch_size=encoder_outputs[1]
+            src_len=encoder_outputs.shape[0]
+
+            # repeat decoder hidden state src_len times
+            hidden=hidden.unsqueeze(1).repeat(1, src_len, 1)
